61db42d854f166338246527ed442a579fa0c7d9a
Mohitsingal <mohitsingal2005@gmail.com>
2025-09-15 18:22:35 +0530

    Initial backend server setup with Express

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('Online Courier Booking API is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
